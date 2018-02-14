39402ca4d54522f337282ff47c7d68183ada3ff8
Sara Abbasi <sara@shecod.es>
2018-02-14 15:28:51 -0500

    node and gulp added

---
+++ wp-content/themes/NASA/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano'); 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+var rename = require('gulp-rename');
+
+gulp.task('default', function() {
+
+});
